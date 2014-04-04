a6540e05cab7dac2c056f977dd60805bf6b588ab
Henning Rauch <Henning@RauchEntwicklung.biz>
2014-04-04 11:05:18 -0700

    added test for graph element template size

--- graphStore/tests/t_graphStore_basic.cxx
+++ graphStore/tests/t_graphStore_basic.cxx
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
+#include <iostream>
 
 #include "graphStore_tests.h"
 
@@ -16,29 +17,28 @@ TEST(test_graphStore_basic_1) {
 }
 
 TEST(test_graphStore_basic_2) {
-        assert(1 > 0);
+	assert(1 > 0);
 
-        return 0;
+	return 0;
+}
+
+TEST(test_graphStore_graphElement_size) {
+
+	assert(sizeof(Vertex<long, long, long>) > sizeof(Vertex<short, short, short>));
+	assert(sizeof(Vertex<long, long, long>) > sizeof(Vertex<short, short, short>));
+
+	return 0;
 }
 
 int main() {
 	int err = 0;
 
-	GraphStore
-	<
-		long long,
-		long,
-		std::string
-	>
-	*gs = new GraphStore
-	<
-		long long,
-		long,
-		std::string
-	>(1000);
+	GraphStore<long long, long, std::string> *gs = new GraphStore<long long,
+			long, std::string>(1000);
 
 	test_graphStore_basic_1(gs);
 	test_graphStore_basic_2(gs);
+	test_graphStore_graphElement_size(gs);
 
 	return err ? -1 : 0;
 }
